38120046ef26c2b9f078867dc0dd583d73790c45
Ismailov Shuhrat <admin@ismax.ru>
2017-03-06 18:03:09 +0300

    Правила для ESLint

--- babel.js
+++ babel.js
@@ -7,6 +7,7 @@ var config;
 try {
     config = JSON.parse(babelrc);
 } catch (err) {
+    /* eslint no-console: ["error", { allow: ["error"] }] */
     console.error('Error parsing .babelrc.');
     console.error(err);
 }
