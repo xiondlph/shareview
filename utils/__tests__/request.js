5fe458892565b913fa9afb1cc41344d249064960
Ismailov Shuhrat <admin@ismax.ru>
2017-03-16 18:54:08 +0300

    Рефакторинг теста утилиты Request

--- utils/__tests__/request.js
+++ utils/__tests__/request.js
@@ -4,6 +4,7 @@
 
 import httpMocks from 'node-mocks-http';
 
+/* eslint max-len: ["error", 130] */
 describe('Тестирование метода api', () => {
     it('Успешное выполнение метода api', (done) => {
         const
@@ -32,24 +33,47 @@ describe('Тестирование метода api', () => {
                         };
 
                         this.end = () => {
-                            if (
-                                opt.port !== 3000 ||
-                                opt.path !== 'url' ||
-                                opt.method !== 'GET' ||
-                                !opt.headers ||
-                                opt.headers.Host !== 'market.icsystem.ru' ||
-                                opt.headers['X-Ismax-Key'] !== '08e212273409793c3199f9cf1a02e2261f78dfd5bb4e5c8776a48299cab0041f' ||
-                                opt.headers['X-Forwarded-Proto'] !== 'http' ||
-                                opt.headers['X-Forwarded-for'] !== '127.0.0.1'
-                            ) {
-                                requestEventStack.error(Error('Request options invalid'));
-                            } else {
-                                responseEventStack.data('Simple');
-                                responseEventStack.data(' response');
-                                responseEventStack.end();
-                            }
+                            responseEventStack.data('Simple');
+                            responseEventStack.data(' response');
+                            responseEventStack.end();
                         };
                     };
+
+                    if (opt.host !== '127.0.0.1') {
+                        throw new Error('api.request.error opt property invalid "host"');
+                    }
+
+                    if (opt.port !== 3000) {
+                        throw new Error('api.request.error opt property invalid "port"');
+                    }
+
+                    if (opt.path !== 'url') {
+                        throw new Error('api.request.error opt property invalid "path"');
+                    }
+
+                    if (opt.method !== 'GET') {
+                        throw new Error('api.request.error opt property invalid "method"');
+                    }
+
+                    if (!opt.headers) {
+                        throw new Error('api.request.error opt property invalid "headers"');
+                    }
+
+                    if (opt.headers.Host !== 'market.icsystem.ru') {
+                        throw new Error('api.request.error opt property invalid "headers.Host"');
+                    }
+
+                    if (opt.headers['X-Ismax-Key'] !== '08e212273409793c3199f9cf1a02e2261f78dfd5bb4e5c8776a48299cab0041f') {
+                        throw new Error('api.request.error opt property invalid "headers[X-Ismax-Key]"');
+                    }
+
+                    if (opt.headers['X-Forwarded-Proto'] !== 'http') {
+                        throw new Error('api.request.error opt property invalid "headers[X-Forwarded-Proto]"');
+                    }
+
+                    if (opt.headers['X-Forwarded-for'] !== '127.0.0.1') {
+                        throw new Error('api.request.error opt property invalid "headers[X-Forwarded-for]"');
+                    }
 
                     return new Request();
                 },
@@ -95,22 +119,45 @@ describe('Тестирование метода api', () => {
                         };
 
                         this.end = () => {
-                            if (
-                                opt.port !== 3000 ||
-                                opt.path !== 'url' ||
-                                opt.method !== 'GET' ||
-                                !opt.headers ||
-                                opt.headers.Host !== 'market.icsystem.ru' ||
-                                opt.headers['X-Ismax-Key'] !== '08e212273409793c3199f9cf1a02e2261f78dfd5bb4e5c8776a48299cab0041f' ||
-                                opt.headers['X-Forwarded-Proto'] !== 'http' ||
-                                opt.headers['X-Forwarded-for'] !== '127.0.0.1'
-                            ) {
-                                requestEventStack.error(Error('Request options invalid'));
-                            } else {
-                                requestEventStack.error(Error('api - error'));
-                            }
+                            requestEventStack.error(Error('api.error'));
                         };
                     };
+
+                    if (opt.host !== '127.0.0.1') {
+                        throw new Error('api.request.error opt property invalid "host"');
+                    }
+
+                    if (opt.port !== 3000) {
+                        throw new Error('api.request.error opt property invalid "port"');
+                    }
+
+                    if (opt.path !== 'url') {
+                        throw new Error('api.request.error opt property invalid "path"');
+                    }
+
+                    if (opt.method !== 'GET') {
+                        throw new Error('api.request.error opt property invalid "method"');
+                    }
+
+                    if (!opt.headers) {
+                        throw new Error('api.request.error opt property invalid "headers"');
+                    }
+
+                    if (opt.headers.Host !== 'market.icsystem.ru') {
+                        throw new Error('api.request.error opt property invalid "headers.Host"');
+                    }
+
+                    if (opt.headers['X-Ismax-Key'] !== '08e212273409793c3199f9cf1a02e2261f78dfd5bb4e5c8776a48299cab0041f') {
+                        throw new Error('api.request.error opt property invalid "headers[X-Ismax-Key]"');
+                    }
+
+                    if (opt.headers['X-Forwarded-Proto'] !== 'http') {
+                        throw new Error('api.request.error opt property invalid "headers[X-Forwarded-Proto]"');
+                    }
+
+                    if (opt.headers['X-Forwarded-for'] !== '127.0.0.1') {
+                        throw new Error('api.request.error opt property invalid "headers[X-Forwarded-for]"');
+                    }
 
                     return new Request();
                 },
@@ -121,7 +168,7 @@ describe('Тестирование метода api', () => {
 
         utils.request.api(req, res, () => {
             req.api('url', (err, status, data) => {
-                expect(err.message).toEqual('api - error');
+                expect(err.message).toEqual('api.error');
                 expect(status).toBeUndefined();
                 expect(data).toBeUndefined();
                 done();
