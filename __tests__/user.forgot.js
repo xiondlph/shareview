8d0a2b14178898f7e20c47503c431ca68573136a
Ismailov Shuhrat <admin@ismax.ru>
2017-03-30 23:29:46 +0300

    Интеграционный тест для API метода /user/forgot (Законченный вариант)

--- __tests__/user.forgot.js
+++ __tests__/user.forgot.js
@@ -4,6 +4,7 @@
 
 jest.mock('../exception');
 
+/* eslint max-len: ["error", 130] */
 describe('Сброс пароля (/user/forgot) - ', () => {
     beforeEach(() => {
         jest.resetModules();
@@ -142,6 +143,355 @@ describe('Сброс пароля (/user/forgot) - ', () => {
                 .end((err, res) => {
                     expect(err).toBeNull();
                     expect(res.body.success).toEqual(false);
+
+                    http.then(server => {
+                        server.close(() => {
+                            done();
+                        });
+                    });
+                });
+        });
+    });
+
+    it('Ошибка в req.model.user.getUserBySession', done => {
+        const
+            supertest = require('supertest'),
+            http = require('../server').default,
+            request = http.then(server => {
+                return supertest.agent(server);
+            });
+
+        jest.mock('../db', () => {
+            return {
+                init: new Promise((resolve) => {
+                    resolve();
+                }),
+
+                db: {
+                    collection: jest.fn()
+                        // Mock для req.model.user.getUserBySession
+                        .mockImplementationOnce(() => {
+                            return {
+                                find() {
+                                    return {
+                                        limit() {
+                                            return {
+                                                toArray() {
+                                                    return new Promise((resolve, reject) => {
+                                                        reject(Error('req.model.user.getUserBySession.error'));
+                                                    });
+                                                },
+                                            };
+                                        },
+                                    };
+                                },
+                            };
+                        }),
+                },
+            };
+        });
+
+        request.then(agent => {
+            agent
+                .post('/user/forgot')
+                .send({ email: 'fake@user.ru' })
+                .expect(500)
+                .end((err, res) => {
+                    expect(err).toBeNull();
+                    expect(res.body.errors).toEqual(['Internal server error']);
+
+                    http.then(server => {
+                        server.close(() => {
+                            done();
+                        });
+                    });
+                });
+        });
+    });
+
+    it('Ошибка валидации email', done => {
+        const
+            supertest = require('supertest'),
+            http = require('../server').default,
+            request = http.then(server => {
+                return supertest.agent(server);
+            });
+
+        jest.mock('../db', () => {
+            return {
+                init: new Promise((resolve) => {
+                    resolve();
+                }),
+
+                db: {
+                    collection: jest.fn()
+                    // Mock для req.model.user.getUserBySession
+                        .mockImplementationOnce(() => {
+                            return {
+                                find() {
+                                    return {
+                                        limit() {
+                                            return {
+                                                toArray() {
+                                                    return new Promise(resolve => {
+                                                        resolve([]);
+                                                    });
+                                                },
+                                            };
+                                        },
+                                    };
+                                },
+                            };
+                        }),
+                },
+            };
+        });
+
+        request.then(agent => {
+            agent
+                .post('/user/forgot')
+                .send({ email: 'fake.invalid.email' })
+                .expect(500)
+                .end((err, res) => {
+                    expect(err).toBeNull();
+                    expect(res.body.errors).toEqual(['Internal server error']);
+
+                    http.then(server => {
+                        server.close(() => {
+                            done();
+                        });
+                    });
+                });
+        });
+    });
+
+    it('Ошибка в req.model.user.getUserBySession', done => {
+        const
+            supertest = require('supertest'),
+            http = require('../server').default,
+            request = http.then(server => {
+                return supertest.agent(server);
+            });
+
+        jest.mock('../db', () => {
+            return {
+                init: new Promise((resolve) => {
+                    resolve();
+                }),
+
+                db: {
+                    collection: jest.fn()
+                        // Mock для req.model.user.getUserBySession
+                        .mockImplementationOnce(() => {
+                            return {
+                                find() {
+                                    return {
+                                        limit() {
+                                            return {
+                                                toArray() {
+                                                    return new Promise(resolve => {
+                                                        resolve([]);
+                                                    });
+                                                },
+                                            };
+                                        },
+                                    };
+                                },
+                            };
+                        })
+
+                        // Mock для req.model.user.setPasswordByEmail
+                        .mockImplementationOnce(() => {
+                            return {
+                                updateOne() {
+                                    return new Promise((resolve, reject) => {
+                                        reject(Error('req.model.user.setPasswordByEmail.error'));
+                                    });
+                                },
+                            };
+                        }),
+                },
+            };
+        });
+
+        request.then(agent => {
+            agent
+                .post('/user/forgot')
+                .send({ email: 'fake@user.ru' })
+                .expect(500)
+                .end((err, res) => {
+                    expect(err).toBeNull();
+                    expect(res.body.errors).toEqual(['Internal server error']);
+
+                    http.then(server => {
+                        server.close(() => {
+                            done();
+                        });
+                    });
+                });
+        });
+    });
+
+    it('Отсутствие затронутых записей в БД', done => {
+        const
+            supertest = require('supertest'),
+            http = require('../server').default,
+            request = http.then(server => {
+                return supertest.agent(server);
+            });
+
+        jest.mock('../db', () => {
+            return {
+                init: new Promise((resolve) => {
+                    resolve();
+                }),
+
+                db: {
+                    collection: jest.fn()
+                        // Mock для req.model.user.getUserBySession
+                        .mockImplementationOnce(() => {
+                            return {
+                                find() {
+                                    return {
+                                        limit() {
+                                            return {
+                                                toArray() {
+                                                    return new Promise(resolve => {
+                                                        resolve([]);
+                                                    });
+                                                },
+                                            };
+                                        },
+                                    };
+                                },
+                            };
+                        })
+
+                        // Mock для req.model.user.setPasswordByEmail
+                        .mockImplementationOnce(() => {
+                            return {
+                                updateOne() {
+                                    return new Promise(resolve => {
+                                        resolve({
+                                            mongoResult: {
+                                                result: {
+                                                    nModified: 0,
+                                                },
+                                            },
+                                        });
+                                    });
+                                },
+                            };
+                        }),
+                },
+            };
+        });
+
+        jest.mock('nodemailer', () => {
+            return {
+                createTransport() {
+                    return {
+                        sendMail(opt, cb) {
+                            cb();
+                        },
+                    };
+                },
+            };
+        });
+
+        request.then(agent => {
+            agent
+                .post('/user/forgot')
+                .send({ email: 'fake@user.ru' })
+                .expect(200)
+                .end((err, res) => {
+                    expect(err).toBeNull();
+                    expect(res.body.success).toEqual(false);
+
+                    http.then(server => {
+                        server.close(() => {
+                            done();
+                        });
+                    });
+                });
+        });
+    });
+
+    it('Ошибка в req.email', done => {
+        const
+            supertest = require('supertest'),
+            http = require('../server').default,
+            request = http.then(server => {
+                return supertest.agent(server);
+            });
+
+        jest.mock('../db', () => {
+            return {
+                init: new Promise((resolve) => {
+                    resolve();
+                }),
+
+                db: {
+                    collection: jest.fn()
+                        // Mock для req.model.user.getUserBySession
+                        .mockImplementationOnce(() => {
+                            return {
+                                find() {
+                                    return {
+                                        limit() {
+                                            return {
+                                                toArray() {
+                                                    return new Promise(resolve => {
+                                                        resolve([]);
+                                                    });
+                                                },
+                                            };
+                                        },
+                                    };
+                                },
+                            };
+                        })
+
+                        // Mock для req.model.user.setPasswordByEmail
+                        .mockImplementationOnce(() => {
+                            return {
+                                updateOne() {
+                                    return new Promise(resolve => {
+                                        resolve({
+                                            mongoResult: {
+                                                result: {
+                                                    nModified: 1,
+                                                },
+                                            },
+                                        });
+                                    });
+                                },
+                            };
+                        }),
+                },
+            };
+        });
+
+        jest.mock('nodemailer', () => {
+            return {
+                createTransport() {
+                    return {
+                        sendMail(opt, cb) {
+                            cb(Error('req.email.error'));
+                        },
+                    };
+                },
+            };
+        });
+
+        request.then(agent => {
+            agent
+                .post('/user/forgot')
+                .send({ email: 'fake@user.ru' })
+                .expect(500)
+                .end((err, res) => {
+                    expect(err).toBeNull();
+                    expect(res.body.errors).toEqual(['Internal server error']);
 
                     http.then(server => {
                         server.close(() => {
