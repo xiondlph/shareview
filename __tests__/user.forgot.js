316ce8770ca23924557c2990cd778ed8a3e74b48
Ismailov Shuhrat <admin@ismax.ru>
2017-03-30 18:15:37 +0300

    Интеграционный тест для API метода /user/forgot
    Дополненный, незаконченный

--- __tests__/user.forgot.js
+++ __tests__/user.forgot.js
@@ -1,98 +1,149 @@
-import supertest from 'supertest';
-import http from '../server';
+/**
+ * Интеграционный тест для API метода /user/forgot
+ */
 
 jest.mock('../exception');
 
-jest.mock('../db', () => {
-    return {
-        init: new Promise((resolve) => {
-            resolve();
-        }),
-
-        db: {
-            collection: jest.fn()
-                .mockImplementationOnce(() => {
-                    return {
-                        find() {
-                            return {
-                                limit() {
-                                    return {
-                                        toArray() {
-                                            return new Promise(resolve => {
-                                                resolve([]);
-                                            });
-                                        },
-                                    };
-                                },
-                            };
-                        },
-                    };
-                })
-                .mockImplementationOnce(() => {
-                    return {
-                        find() {
-                            return {
-                                limit() {
-                                    return {
-                                        toArray() {
-                                            return new Promise(resolve => {
-                                                resolve([]);
-                                            });
-                                        },
-                                    };
-                                },
-                            };
-                        },
-                    };
-                })
-                .mockImplementationOnce(() => {
-                    return {
-                        insertOne() {
-                            return new Promise(resolve => {
-                                resolve({
-                                    insertedId: 'fake.inserted.id',
-                                });
-                            });
-                        },
-                    };
-                }),
-        },
-    }
-});
-
-jest.mock('nodemailer', () => {
-    return {
-        createTransport() {
-            return {
-                sendMail(opt, cb) {
-                    cb();
-                },
-            };
-        },
-    };
-});
-
-const request = http.then(server => {
-    return supertest.agent(server);
-});
-
-describe('Регистрация пользователя (/user/create) - ', () => {
-    afterAll(() => {
-        http.then(server => {
-            server.close();
-        });
+describe('Сброс пароля (/user/forgot) - ', () => {
+    beforeEach(() => {
+        jest.resetModules();
     });
 
-    it('Успешная регистрация', done => {
+    it('Успешный сброс', done => {
+        const
+            supertest = require('supertest'),
+            http = require('../server').default,
+            request = http.then(server => {
+                return supertest.agent(server);
+            });
+
+        jest.mock('../db', () => {
+            return {
+                init: new Promise((resolve) => {
+                    resolve();
+                }),
+
+                db: {
+                    collection: jest.fn()
+                        .mockImplementationOnce(() => {
+                            return {
+                                find() {
+                                    return {
+                                        limit() {
+                                            return {
+                                                toArray() {
+                                                    return new Promise(resolve => {
+                                                        resolve([]);
+                                                    });
+                                                },
+                                            };
+                                        },
+                                    };
+                                },
+                            };
+                        })
+                        .mockImplementationOnce(() => {
+                            return {
+                                updateOne(query, data) {
+                                    return new Promise(resolve => {
+                                        resolve({
+                                            mongoResult: {
+                                                result: {
+                                                    nModified: 1,
+                                                },
+                                            },
+                                        });
+                                    });
+                                },
+                            };
+                        }),
+                },
+            }
+        });
+
+        jest.mock('nodemailer', () => {
+            return {
+                createTransport() {
+                    return {
+                        sendMail(opt, cb) {
+                            cb();
+                        },
+                    };
+                },
+            };
+        });
+
         request.then(agent => {
             agent
-                .post('/user/create')
+                .post('/user/forgot')
                 .send({ email: 'fake@user.ru' })
                 .expect(200)
                 .end((err, res) => {
                     expect(err).toBeNull();
                     expect(res.body.success).toEqual(true);
-                    done();
+
+                    http.then(server => {
+                        server.close(() => {
+                            done();
+                        });
+                    });
+                });
+        });
+    });
+
+    it('Если пользователь авторизован', done => {
+        const
+            supertest = require('supertest'),
+            http = require('../server').default,
+            request = http.then(server => {
+                return supertest.agent(server);
+            });
+
+        jest.mock('../db', () => {
+            return {
+                init: new Promise((resolve) => {
+                    resolve();
+                }),
+
+                db: {
+                    collection: jest.fn()
+                        .mockImplementationOnce(() => {
+                            return {
+                                find() {
+                                    return {
+                                        limit() {
+                                            return {
+                                                toArray() {
+                                                    return new Promise(resolve => {
+                                                        resolve([{
+                                                            email: 'fake@user.com',
+                                                        }]);
+                                                    });
+                                                },
+                                            };
+                                        },
+                                    };
+                                },
+                            };
+                        }),
+                },
+            }
+        });
+
+        request.then(agent => {
+            agent
+                .post('/user/forgot')
+                .send({ email: 'fake@user.ru' })
+                .expect(200)
+                .end((err, res) => {
+                    expect(err).toBeNull();
+                    expect(res.body.success).toEqual(false);
+
+                    http.then(server => {
+                        server.close(() => {
+                            done();
+                        });
+                    });
                 });
         });
     });
