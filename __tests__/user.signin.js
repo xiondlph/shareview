a54c457d66daa29a86e10aa2f910ac05e0a8b71f
Ismailov Shuhrat <admin@ismax.ru>
2017-04-03 00:41:29 +0300

    Интеграционный тест для API метода /user/signin

---
+++ __tests__/user.signin.js
@@ -0,0 +1,467 @@
+/**
+ * Интеграционный тест для API метода /user/signin
+ */
+
+jest.mock('../exception');
+
+/* eslint max-len: ["error", 140] */
+describe('Авторизация пользователя (/user/signin) - ', () => {
+    beforeEach(() => {
+        jest.resetModules();
+    });
+
+    it('Успешная авторизация', done => {
+        const
+            supertest = require('supertest'),
+            http = require('../server').default,
+            request = http.then(server => {
+                return supertest.agent(server);
+            });
+
+        jest.mock('../db', () => {
+            return {
+                init: new Promise((resolve) => {
+                    resolve();
+                }),
+
+                db: {
+                    collection: jest.fn()
+                        // Mock для req.model.user.getUserBySession
+                        .mockImplementationOnce(() => {
+                            return {
+                                find() {
+                                    return {
+                                        limit() {
+                                            return {
+                                                toArray() {
+                                                    return new Promise(resolve => {
+                                                        resolve([]);
+                                                    });
+                                                },
+                                            };
+                                        },
+                                    };
+                                },
+                            };
+                        })
+
+                        // Mock для req.model.user.getUserByEmail
+                        .mockImplementationOnce(() => {
+                            return {
+                                find() {
+                                    return {
+                                        limit() {
+                                            return {
+                                                toArray() {
+                                                    return new Promise(resolve => {
+                                                        resolve([{
+                                                            _id: 'fake_id',
+
+                                                            // Захешеный "fake.password"
+                                                            password: 'cf1a63230b18ca6299394958b885096d0fe86c85b3870364706d488e57788e4a',
+
+                                                            email: 'fake.email',
+                                                            address: 'fake.address',
+                                                            salt: 'fake.salt',
+                                                            period: 'fake.period',
+                                                        }]);
+                                                    });
+                                                },
+                                            };
+                                        },
+                                    };
+                                },
+                            };
+                        })
+
+                        // Mock для req.model.user.setSessionById
+                        .mockImplementationOnce(() => {
+                            return {
+                                updateOne() {
+                                    return new Promise(resolve => {
+                                        resolve();
+                                    });
+                                },
+                            };
+                        }),
+                },
+            };
+        });
+
+        request.then(agent => {
+            agent
+                .post('/user/signin')
+                .send({
+                    email: 'fake@email.ru',
+                    password: 'fake.password',
+                })
+                .expect(200)
+                .end((err, res) => {
+                    expect(err).toBeNull();
+                    expect(res.body.success).toEqual(true);
+
+                    http.then(server => {
+                        server.close(() => {
+                            done();
+                        });
+                    });
+                });
+        });
+    });
+
+    it('Если пользователь авторизован', done => {
+        const
+            supertest = require('supertest'),
+            http = require('../server').default,
+            request = http.then(server => {
+                return supertest.agent(server);
+            });
+
+        jest.mock('../db', () => {
+            return {
+                init: new Promise((resolve) => {
+                    resolve();
+                }),
+
+                db: {
+                    collection: jest.fn()
+                        // Mock для req.model.user.getUserBySession
+                        .mockImplementationOnce(() => {
+                            return {
+                                find() {
+                                    return {
+                                        limit() {
+                                            return {
+                                                toArray() {
+                                                    return new Promise(resolve => {
+                                                        resolve([{
+                                                            email: 'fake@user.com',
+                                                        }]);
+                                                    });
+                                                },
+                                            };
+                                        },
+                                    };
+                                },
+                            };
+                        }),
+                },
+            };
+        });
+
+        request.then(agent => {
+            agent
+                .post('/user/signin')
+                .send({
+                    email: 'fake@email.ru',
+                    password: 'fake.password',
+                })
+                .expect(200)
+                .end((err, res) => {
+                    expect(err).toBeNull();
+                    expect(res.body.success).toEqual(true);
+
+                    http.then(server => {
+                        server.close(() => {
+                            done();
+                        });
+                    });
+                });
+        });
+    });
+
+    it('req.model.user.getUserBySession', done => {
+        const
+            supertest = require('supertest'),
+            http = require('../server').default,
+            request = http.then(server => {
+                return supertest.agent(server);
+            });
+
+        jest.mock('../db', () => {
+            return {
+                init: new Promise((resolve) => {
+                    resolve();
+                }),
+
+                db: {
+                    collection: jest.fn()
+                        // Mock для req.model.user.getUserBySession
+                        .mockImplementationOnce(() => {
+                            return {
+                                find() {
+                                    return {
+                                        limit() {
+                                            return {
+                                                toArray() {
+                                                    return new Promise((resolve, reject) => {
+                                                        reject(Error('req.model.user.getUserBySession.error'));
+                                                    });
+                                                },
+                                            };
+                                        },
+                                    };
+                                },
+                            };
+                        }),
+                },
+            };
+        });
+
+        request.then(agent => {
+            agent
+                .post('/user/signin')
+                .send({
+                    email: 'fake@email.ru',
+                    password: 'fake.password',
+                })
+                .expect(500)
+                .end((err, res) => {
+                    expect(err).toBeNull();
+                    expect(res.body.errors).toEqual(['Internal server error']);
+
+                    http.then(server => {
+                        server.close(() => {
+                            done();
+                        });
+                    });
+                });
+        });
+    });
+
+    it('Ошибка валидации email', done => {
+        const
+            supertest = require('supertest'),
+            http = require('../server').default,
+            request = http.then(server => {
+                return supertest.agent(server);
+            });
+
+        jest.mock('../db', () => {
+            return {
+                init: new Promise((resolve) => {
+                    resolve();
+                }),
+
+                db: {
+                    collection: jest.fn()
+                        // Mock для req.model.user.getUserBySession
+                        .mockImplementationOnce(() => {
+                            return {
+                                find() {
+                                    return {
+                                        limit() {
+                                            return {
+                                                toArray() {
+                                                    return new Promise(resolve => {
+                                                        resolve([]);
+                                                    });
+                                                },
+                                            };
+                                        },
+                                    };
+                                },
+                            };
+                        }),
+                },
+            };
+        });
+
+        request.then(agent => {
+            agent
+                .post('/user/signin')
+                .send({
+                    email: 'fake.invalid.email',
+                    password: 'fake.password',
+                })
+                .expect(500)
+                .end((err, res) => {
+                    expect(err).toBeNull();
+                    expect(res.body.errors).toEqual(['Internal server error']);
+
+                    http.then(server => {
+                        server.close(() => {
+                            done();
+                        });
+                    });
+                });
+        });
+    });
+
+    it('Ошибка в req.model.user.getUserByEmail', done => {
+        const
+            supertest = require('supertest'),
+            http = require('../server').default,
+            request = http.then(server => {
+                return supertest.agent(server);
+            });
+
+        jest.mock('../db', () => {
+            return {
+                init: new Promise((resolve) => {
+                    resolve();
+                }),
+
+                db: {
+                    collection: jest.fn()
+                    // Mock для req.model.user.getUserBySession
+                        .mockImplementationOnce(() => {
+                            return {
+                                find() {
+                                    return {
+                                        limit() {
+                                            return {
+                                                toArray() {
+                                                    return new Promise(resolve => {
+                                                        resolve([]);
+                                                    });
+                                                },
+                                            };
+                                        },
+                                    };
+                                },
+                            };
+                        })
+
+                        // Mock для req.model.user.getUserByEmail
+                        .mockImplementationOnce(() => {
+                            return {
+                                find() {
+                                    return {
+                                        limit() {
+                                            return {
+                                                toArray() {
+                                                    return new Promise((resolve, reject) => {
+                                                        reject(Error('req.model.user.getUserByEmail.error'));
+                                                    });
+                                                },
+                                            };
+                                        },
+                                    };
+                                },
+                            };
+                        }),
+                },
+            };
+        });
+
+        request.then(agent => {
+            agent
+                .post('/user/signin')
+                .send({
+                    email: 'fake@email.ru',
+                    password: 'fake.password',
+                })
+                .expect(500)
+                .end((err, res) => {
+                    expect(err).toBeNull();
+                    expect(res.body.errors).toEqual(['Internal server error']);
+
+                    http.then(server => {
+                        server.close(() => {
+                            done();
+                        });
+                    });
+                });
+        });
+    });
+
+    it('Ошибка в req.model.user.setSessionById', done => {
+        const
+            supertest = require('supertest'),
+            http = require('../server').default,
+            request = http.then(server => {
+                return supertest.agent(server);
+            });
+
+        jest.mock('../db', () => {
+            return {
+                init: new Promise((resolve) => {
+                    resolve();
+                }),
+
+                db: {
+                    collection: jest.fn()
+                    // Mock для req.model.user.getUserBySession
+                        .mockImplementationOnce(() => {
+                            return {
+                                find() {
+                                    return {
+                                        limit() {
+                                            return {
+                                                toArray() {
+                                                    return new Promise(resolve => {
+                                                        resolve([]);
+                                                    });
+                                                },
+                                            };
+                                        },
+                                    };
+                                },
+                            };
+                        })
+
+                        // Mock для req.model.user.getUserByEmail
+                        .mockImplementationOnce(() => {
+                            return {
+                                find() {
+                                    return {
+                                        limit() {
+                                            return {
+                                                toArray() {
+                                                    return new Promise(resolve => {
+                                                        resolve([{
+                                                            _id: 'fake_id',
+
+                                                            // Захешеный "fake.password"
+                                                            password: 'cf1a63230b18ca6299394958b885096d0fe86c85b3870364706d488e57788e4a',
+
+                                                            email: 'fake.email',
+                                                            address: 'fake.address',
+                                                            salt: 'fake.salt',
+                                                            period: 'fake.period',
+                                                        }]);
+                                                    });
+                                                },
+                                            };
+                                        },
+                                    };
+                                },
+                            };
+                        })
+
+                        // Mock для req.model.user.setSessionById
+                        .mockImplementationOnce(() => {
+                            return {
+                                updateOne() {
+                                    return new Promise((resolve, reject) => {
+                                        reject(Error('req.model.user.setSessionById.error'));
+                                    });
+                                },
+                            };
+                        }),
+                },
+            };
+        });
+
+        request.then(agent => {
+            agent
+                .post('/user/signin')
+                .send({
+                    email: 'fake@email.ru',
+                    password: 'fake.password',
+                })
+                .expect(500)
+                .end((err, res) => {
+                    expect(err).toBeNull();
+                    expect(res.body.errors).toEqual(['Internal server error']);
+
+                    http.then(server => {
+                        server.close(() => {
+                            done();
+                        });
+                    });
+                });
+        });
+    });
+});
